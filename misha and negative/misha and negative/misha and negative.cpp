08ff8f19c0275963b024b24509bc68db920a3bd3
New-Skytech <76553267+New-Skytech@users.noreply.github.com>
2022-11-11 22:49:22 +0500

    Add files via upload

---
+++ misha and negative/misha and negative/misha and negative.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	string original[100][100];
+	for (int x_temp = 0; x_temp < 100; x_temp++) {
+		for (int y_temp = 0; y_temp < 100; y_temp++) {
+			original[x_temp][y_temp] = 'F';
+
+		}
+	}
+	string neg[100][100];
+	for (int x_temp = 0; x_temp < 100; x_temp++) {
+		for (int y_temp = 0; y_temp < 100; y_temp++) {
+			neg[x_temp][y_temp] = 'F';
+
+		}
+	}
+
+	int x, y;
+	int diference = 0;
+	string path = "INPUT.txt";
+	string str;
+
+
+	ifstream fin;
+	fin.open(path);
+	fin >> x;
+	fin >> y;
+	str = fin.get();
+
+	for (int x_temp = 0; x_temp < x; x_temp++){
+		for (int y_temp = 0; y_temp < y; y_temp++) {
+			original[x_temp][y_temp] = fin.get();
+		}
+		str = fin.get();
+	}
+	str = fin.get();
+	for (int x_temp = 0; x_temp < x; x_temp++) {
+		for (int y_temp = 0; y_temp < y; y_temp++) {
+			neg[x_temp][y_temp] = fin.get();
+		}
+		str = fin.get();
+	}
+
+	for (int x_temp = 0; x_temp < x; x_temp++) {
+		for (int y_temp = 0; y_temp < y; y_temp++) {
+			if (original[x_temp][y_temp] == neg[x_temp][y_temp])
+			{
+				diference += 1;
+			}
+		}
+	}
+	cout << diference;
+}
+
+
---
+++ pattern/pattern/pattern.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	string pattern[4][4];
+	string path = "INPUT.txt";
+	string str;
+
+	bool flag = true;
+
+	ifstream fin;
+	fin.open(path);
+	for (int x_temp = 0; x_temp < 4; x_temp++) {
+		for (int y_temp = 0; y_temp < 4; y_temp++) {
+			pattern[x_temp][y_temp] = fin.get();
+			cout << pattern[x_temp][y_temp];
+		}
+		str = fin.get();
+		cout << endl;
+	}
+
+	for (int x_temp = 0; x_temp < 3; x_temp++) {
+		for (int y_temp = 0; y_temp < 3; y_temp++) {
+			if (pattern[x_temp][y_temp] == pattern[x_temp+1][y_temp] && pattern[x_temp+1][y_temp] == pattern[x_temp][y_temp+1] && pattern[x_temp][y_temp+1] == pattern[x_temp+1][y_temp+1])
+			{
+				flag = false;
+			}
+		}
+	}
+	if (flag == true)
+	{
+		cout << "yes";
+	}
+	else
+	{
+		cout << "no";
+	}
+}
