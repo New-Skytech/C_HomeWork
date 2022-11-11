d3e7ff5c928cc313993e20428bef62c425bfb790
New-Skytech <76553267+New-Skytech@users.noreply.github.com>
2022-11-11 23:57:38 +0500

    Add files via upload

---
+++ sequences/sequences/sequences.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    char x; int ch = 0;
+    string path = "INPUT.txt";
+    ifstream fin;
+    fin.open(path);
+
+    while (fin >> x)
+        if (x == '1') {
+            cout << (char)(97 + ch);
+            ch = 0;
+        }
+        else
+            ch++;
+}
---
+++ words/words/words.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int Fib(int i)
+{
+    int value = 0;
+    if (i < 1) return 0;
+    if (i == 1) return 1;
+    return Fib(i - 1) + Fib(i - 2);
+}
+
+
+int main()
+{
+    int num, cnt = 0;
+    char ch;
+    string str[300];
+    string path = "INPUT.txt";
+    ifstream fin;
+    fin.open(path);
+
+    while(fin.get(ch))
+    {
+        str[cnt] = ch;
+        cnt++;
+    }
+
+    for(int i = 2; i < cnt; i++)
+    {
+        num = Fib(i);
+        if (num > cnt)
+        {
+            break;
+        }
+        cout << str[num-1];
+    }
+}
